8fe3d99e0e9867d219ede42758a1e46dcad74bbc
hugo <mbiz.hugo@gmail.com>
2016-09-08 11:13:06 +0700

    add production.js

---
+++ config/env/production.js
@@ -0,0 +1,19 @@
+module.exports = {
+    db: 'mongodb://fdxfdcfd:thyThy313@ds019766.mlab.com:19766/chobungbu',
+    sessionSecret: 'shhhhh! very secret!!!',
+    facebook: {
+        clientID: '1372118666134846',
+        clientSecret: 'df73e43eee67e2191629a4473c7db749',
+        callbackURL: 'http://localhost:3000/oauth/facebook/callback'
+    },
+    twitter: {
+        clientID: 'lYRMrgAQDoTYrDMg7Exfp9BQa',
+        clientSecret: 'M3v2Yj45dlLoNtNnFaVl4nrym534YdEWJj5UtDjcXyA9FgedPb',
+        callbackURL: 'http://localhost:3000/oauth/twitter/callback'
+    },
+    google: {
+        clientID: '417162800844-oi51bqpcfme8rjo920gn66pbufvtf6oc.apps.googleusercontent.com',
+        clientSecret: '0rKtOaFiv_0h9RhnZ9tlMz6n',
+        callbackURL: 'http://localhost:3000/oauth/google/callback'
+    }
+};
