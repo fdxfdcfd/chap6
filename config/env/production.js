1daf1888427c8d3b2a6b7e362eff0e9b14026057
hugo <mbiz.hugo@gmail.com>
2016-09-09 09:24:05 +0700

    ud

--- config/env/production.js
+++ config/env/production.js
@@ -4,16 +4,16 @@ module.exports = {
     facebook: {
         clientID: '1372118666134846',
         clientSecret: 'df73e43eee67e2191629a4473c7db749',
-        callbackURL: 'https://chap6.herokuapp.com//oauth/facebook/callback'
+        callbackURL: 'https://chap6.herokuapp.com/oauth/facebook/callback'
     },
     twitter: {
         clientID: 'lYRMrgAQDoTYrDMg7Exfp9BQa',
         clientSecret: 'M3v2Yj45dlLoNtNnFaVl4nrym534YdEWJj5UtDjcXyA9FgedPb',
-        callbackURL: 'https://chap6.herokuapp.com//oauth/twitter/callback'
+        callbackURL: 'https://chap6.herokuapp.com/oauth/twitter/callback'
     },
     google: {
         clientID: '417162800844-oi51bqpcfme8rjo920gn66pbufvtf6oc.apps.googleusercontent.com',
         clientSecret: '0rKtOaFiv_0h9RhnZ9tlMz6n',
-        callbackURL: 'https://chap6.herokuapp.com//oauth/google/callback'
+        callbackURL: 'https://chap6.herokuapp.com/oauth/google/callback'
     }
 };
